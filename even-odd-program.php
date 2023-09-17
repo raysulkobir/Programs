2b54b5853bd02e2cf20088b565d7220b7428621e
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-09-17 21:34:16 +0600

    add event and doo number

---
+++ even-odd-program.php
@@ -0,0 +1,12 @@
+<?php
+//  Even Odd Program
+
+$numbers = [10, 20, 30, 40, 50, 51];
+
+foreach ($numbers as $number) {
+    if ($number % 2) {
+        echo "$number is Even Number \n";
+    } else {
+        echo "$number is Odd Number \n";
+    }
+}
