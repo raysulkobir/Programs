1ff5411625aeed9d0eaa5ae39bce71cd8b2a54e2
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-09-17 18:53:19 +0600

    start program

---
+++ sum-of-digits.php
@@ -0,0 +1,14 @@
+<?php
+
+// To find sum of digits of a number just add all the digits.
+
+$num = 14597;
+$sum = 0;
+$rem = 0;
+for ($i = 0; $i <= strlen($num); $i++) {
+    $rem = $num % 10;
+    $sum = $sum + $rem;
+    $num = $num / 10;
+}
+
+echo "Sum of digits 14597 is $sum";
